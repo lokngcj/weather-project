7e79150d2aee7d97dab769772caf5102f16ecdcc
zyx <lookngcj@163.com>
2026-03-24 16:27:17 +0800

    add mock weather data

--- main.py
+++ main.py
@@ -7,12 +7,27 @@ def get_city_input():
         else:
             return city
 
-def print_city(city):
-    print(f"你查询的城市是: {city}")
+def get_weather(city):
+    weather_data = {
+        "beijing": "晴朗，温度25°C",
+        "shanghai": "多云，温度28°C",
+        "guangzhou": "雨天，温度30°C",
+        "shenzhen": "晴朗，温度27°C"
+    }
+    city_key = city.lower()
+
+    if city_key in weather_data:
+        return weather_data[city_key]
+    else:
+        return "暂无该城市的天气信息。"
+
+def print_weather(city,weather):
+    print(f"{city}的天气是: {weather}")
 
 def main():
     city = get_city_input()
-    print_city(city)
+    weather = get_weather(city)
+    print_weather(city, weather)
 
 if __name__ == "__main__":
     main()
