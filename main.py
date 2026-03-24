c62189e7bba0701e3a55b9b5ef30209442d5fa88
zyx <lookngcj@163.com>
2026-03-24 16:10:09 +0800

    refactor code int functions

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-city = input("Enter the city name: ")
-print(f"你查询的城市是: {city}")
+
+def get_city_input():
+    city = input("Enter the city name: ")
+    return city
+
+def print_city(city):
+    print(f"你查询的城市是: {city}")
+
+def main():
+    city = get_city_input()
+    print_city(city)
+
+if __name__ == "__main__":
+    main()
